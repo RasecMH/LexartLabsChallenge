3d5a3a330fac1385d75c506cbdaf052d398b55c9
RasecMH <rasecmh0@gmail.com>
2023-04-11 21:53:19 -0300

    🧪 test(SearchProducts): adding base tests

---
+++ backend/tests/unit/mock/searchProducts.mock.ts
@@ -0,0 +1,18 @@
+export const productListMock = [
+  {
+    id: 'validId',
+    image: 'image.link.png',
+    description: 'product description',
+    category: 'geladeira',
+    price: '100',
+    store: 'Mercado Livre',
+  },
+  {
+    id: 'validId2',
+    image: 'image2.link.png',
+    description: 'product description 2',
+    category: 'mobile',
+    price: '500',
+    store: 'Buscapé',
+  },
+];
