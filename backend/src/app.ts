2aa1827cbcffeb3324b3f859b1ec50dc808d09b6
RasecMH <rasecmh0@gmail.com>
2023-04-13 02:34:21 -0300

    ✨ feat(*): app connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,18 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { StatusCodes } from 'http-status-codes';
+import SearchProductsRoute from './routes/SearchProductsRoute';
+import ErrorMiddleware from './middlewares/ErrorMiddleware';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+app.get('/', (_req: Request, res: Response) =>
+  res.status(StatusCodes.OK).send('Ok')
+);
+
+app.use(SearchProductsRoute);
+app.use(ErrorMiddleware);
 
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,12 @@
 import 'dotenv/config';
 import app from './app';
+import dbConnection from './models/Connection';
 
 const PORT = process.env.PORT || 3001;
 try {
-  app.listen(PORT, () => console.log(`Running server on port: ${PORT}`));
+  dbConnection().then(() => {
+    app.listen(PORT, () => console.log(`Running server on port: ${PORT}`));
+  });
 } catch (error) {
   console.log('Connection with database generated an error:\r\n');
   console.error(error);
