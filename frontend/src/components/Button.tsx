126d03bc993a22c841828366178d73b00102922b
Cesar Holanda <rasecmh0@gmail.com>
2023-04-14 17:10:04 -0300

    🌈 style(components): Button

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 interface ButtonProps {
   label: string;
-  onClick: () => void;
+  onClick?: () => void;
 }
 
 export default function Button({ label, onClick }: ButtonProps): JSX.Element {
   return (
-    <button type='button' onClick={onClick}>
+    <button
+      className='text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-4 lg:mb-0 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 w-full'
+      type='button'
+      onClick={onClick}>
       {label}
     </button>
   );
